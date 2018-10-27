3008a190a0c25cb6bab67fea4e6134ea8b6e7383
claudiom8 <claudio.morelli@outlook.com>
2018-10-27 09:52:47 +0200

    up

---
+++ Entity/EObject.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'req.php';
+
+/*La classe EObject caratterizza un oggetto Entity a partire dal suo Id.*/
+class EObject
+{
+    /*l'id che identifica l'oggetto.*/
+    protected $id; 
+    
+    /*Costruisce un oggetto vuoto.*/
+    protected function __construct(int $id=null) {
+        $this->id = $id;
+    }
+    
+    /*Ritorna l'identifier dell'oggetto*/
+    function getId() : int {
+        if(!$this->id)
+            return 0;
+        else return $this->id;
+    }
+    
+    /*Setta l'identificativo dell'oggetto Entity*/
+    function setId(int $id) {
+        $this->id = $id;
+    }
+    
+}
+
