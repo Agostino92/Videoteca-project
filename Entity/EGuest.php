fe2e1e5814d0eb77216c8c4a4dccee8d99f87435
claudiom8 <claudio.morelli@outlook.com>
2018-10-27 09:18:44 +0200

    entity

--- Entity/EGuest.php
+++ Entity/EGuest.php
@@ -3,6 +3,11 @@
 /** La classe EGuest estende la classe padre EUser e implementa la tipologia di utente Guest */
 class EGuest extends EUser
 {
+	/*l'id dell'utente*/
+	protected $id;
+	/*l'username dell'utente posto a Visitor*/
+	protected $username
+	
     /** Costruisce un utente Guest. L'id viene posto a 0 e il suo username e' semplicemente 'Visitor' */
     function __construct($id, $username)
     {
