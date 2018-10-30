8f12cfaac1cd6d35d19164f32446816985267fe6
claudiom8 <claudio.morelli@outlook.com>
2018-10-30 16:26:05 +0100

    Update req.php

--- req.php
+++ req.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'Installation.php';
-//require_once 'SampleUsers.php';
+require_once 'SampleUsers.php';
 require_once 'SmartyConfig.php';
 require_once 'Controller/FrontController.php';
 require_once 'Controller/CUser.php';
@@ -12,17 +12,17 @@ require_once 'Controller/CManageReport.php';
 require_once 'Controller/CFollower.php';
 require_once 'Controller/CUserInfo.php';
 require_once 'Controller/CSupInfo.php';
-require_once 'Controller/CSupporter.php';
+require_once 'Controller/CSupporter.php';*/
 require_once 'Controller/CSearch.php';
-require_once 'Controller/CAdmin.php'; */
+/*require_once 'Controller/CAdmin.php'; */
 require_once 'View/VObject.php';
 require_once 'View/VUser.php';
 /*require_once 'View/VReport.php';
 require_once 'View/VSong.php';
 require_once 'View/VUserInfo.php';
-require_once 'View/VSupInfo.php';
+require_once 'View/VSupInfo.php';*/
 require_once 'View/VSearch.php';
-require_once 'View/VSupporter.php';
+/*require_once 'View/VSupporter.php';
 require_once 'View/VAdmin.php'; */
 
 /*require_once 'Entity/EListener.php';
@@ -42,17 +42,17 @@ require_once 'Entity/ESong.php';
 require_once 'Entity/EMp3.php';
 require_once 'Entity/ESupporter.php'; */
 
-require_once 'Foundation/FDatabase.php';
-/*require_once 'Foundation/FTarget.php';
-require_once 'Foundation/FSong.php';
-require_once 'Foundation/FMp3.php';
+/*require_once 'Foundation/FDatabase.php';*/
+require_once 'Foundation/FTarget.php';
+/*require_once 'Foundation/FSong.php';
+require_once 'Foundation/FMp3.php';*/
 require_once 'Foundation/FUser.php';
 require_once 'Foundation/FUserInfo.php';
-require_once 'Foundation/FImg.php';
+/*require_once 'Foundation/FImg.php';
 require_once 'Foundation/FSupInfo.php';
 require_once 'Foundation/FReport.php';
-require_once 'Foundation/FFollower.php';
+require_once 'Foundation/FFollower.php';*/
 require_once 'Foundation/FPersistantManager.php';
-require_once 'Foundation/FSupporter.php'; */
+/*require_once 'Foundation/FSupporter.php'; */
 
 ?>
