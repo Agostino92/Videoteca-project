53281a4b770caab5d5e317da46ddaa6246365a72
claudiom8 <claudio.morelli@outlook.com>
2018-11-02 16:36:26 +0100

    EUser up

--- Entity/EFilm.php
+++ Entity/EFilm.php
@@ -2,12 +2,18 @@
 
 class EFilm 
 {
-    /** Il nome del film */
+    /** Il codice del film */
+    private $codice; 
+	/** Il nome del film */
     private $name;          
     /** L'autore del film */
     private $author;        
     /** Il genere del film */
-    private $genre; 	   
+    private $genre; 	
+    /** La descrizione del film */
+    private $descrizione
+	/** La locandina del film */
+    private $locandina; ; 	
     /** Denota se la recensione del film può essere vista dagli utenti guest */  
     private $guest;
     /** Denota se la recensione del film può essere vista dagli utenti registrati */
@@ -18,7 +24,6 @@ class EFilm
     /* Instanzia un oggetto EFilm vuoto, di default non visibile a nessuna tipologia di utente */
     function __construct ()
     {
-        $this->recensione = new ERecensione();
         $this->author = new EUser();
         $guest = false;
         $supporter = false;
@@ -42,12 +47,30 @@ class EFilm
         return $this->genre;
     }
     
+    /** Metodo che fornisce il codice di un film */
+    function getCodice() 
+    {
+        return $this->codice;
+    }
+	
+	/** Metodo che fornisce la descrizione di un film */
+    function getDescrizione() 
+    {
+        return $this->descrizione;
+    }
+	
+	/** Metodo che fornisce la locandina di un film */
+    function getLocandina() 
+    {
+        return $this->locandina;
+    }
+	
     /** Metodo che fornisce la recensione di un film */
     function getRecensione() 
     {
         return $this->recensione;
     }
-    
+	
     /** Metodo che imposta l'autore del film */
     function setAuthor($author)
     {
@@ -71,7 +94,25 @@ class EFilm
     { 
        $this->recensione = $recensione;
     }
+	
+	/** Metodo che imposta il codice del film */
+    function setCodice($codice)
+    {
+        $this->codice = $codice;
+    }
          
+    /** Metodo che imposta la descrizione del film */
+    function setDescrizione($descrizione)
+    {
+        $this->descrizione = $descrizione;
+    }
+	
+	/** Metodo che imposta la locandina del film */
+    function setLocandina($locandina)
+    {
+        $this->locandina = $locandina;
+    }
+	
     /** Controlla se il film e' visibile per tutte le tipologie di utenti */
     function isForAll() : bool
     {
