a9de688bf7de8f1bd4fc0d7323c8add0b4e1f185
claudiom8 <claudio.morelli@outlook.com>
2018-11-03 09:31:03 +0100

    piccoli cambiamenti Vsearch

--- View/VSearch.php
+++ View/VSearch.php
@@ -21,7 +21,26 @@ class VSearch extends VObject
         }
         return $string;
     }
-           
+        
+    /**
+     * Ritorna la coppia chiave-valore scelta dall'utente nella ricerca avanzata. Tale coppia 
+     * e' contenuta nell'array globale $_GET.
+     * @return array avente come valori la chiave e il valore
+   
+    function getKeyAndValue() : array
+    {
+        $key="";
+        $value="";
+        
+        if($_GET['value'] == 'name' || $_GET['value'] == 'genre')
+            $value = ucfirst($_GET['value']);
+        if($_GET['key'] == 'song' || $_GET['key'] == 'user')
+            $key = ucfirst($_GET['key']);
+                
+        return array($key, $value);
+    }		
+	*/
+	
     /*Mostra i risultati della ricerca
 	EUser $user utente della sessione, $array risultati della ricerca (NULL se nessun oggetto e' stato costruito),
     $key chiave di ricerca adoperata, $value valore di ricerca adoperato, $string dato ricercato dall'utente*/
@@ -38,7 +57,7 @@ class VSearch extends VObject
         $this->smarty->assign('array', $array);
         
         //mostro il contenuto della pagine
-        $this->smarty->display('search/search.tpl');
+        $this->smarty->display('search.tpl');
     }
 }
 
