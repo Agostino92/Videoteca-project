4bf2274832f62699f8c6cb9a231c6ab41fb2a5a2
lotiriel <morelli.massimo0@gmail.com>
2018-11-05 17:48:12 +0100

    Revert "Update Entity/EFilm.php"
    This reverts commit 2465c4669821a0e75723b863f65c34fdb685c747.

--- Foundation/FFilm.php
+++ Foundation/FFilm.php
@@ -35,7 +35,7 @@ class FFilm {
      * Ottieni il/i film dal database ricercati per author
      * @return string la query sql per la SELECT
      */
-    static function searchFilmByAuthor() : string
+    static function loadFilmforAuthor() : string
     {
         return "SELECT * 
                 FROM film
@@ -93,7 +93,8 @@ class FFilm {
         $stmt->bindValue(':genre', $obj->getGenre(), PDO::PARAM_STR);
 		$stmt->bindValue(':descrizione', $obj->getDescrizione(), PDO::PARAM_STR);
 		$stmt->bindValue(':locandina', $obj->getLocandina(), PDO::PARAM_STR);
-		
+		$stmt->bindValue(':forall', (int) $obj->isForAll(), PDO::PARAM_INT);
+        $stmt->bindValue(':registered', (int) $obj->isForRegisteredOnly(), PDO::PARAM_INT);
         
     }
 	
@@ -114,10 +115,6 @@ class FFilm {
         $film->setGenre($row['genre']);
 		$film->setDescrizione($row['descrizione']);
 		$film->setLocandina($row['locandina']);
-<<<<<<< HEAD
-=======
-        
->>>>>>> dbc69086bfacb9878477cfbacb9afcffed180481
         // restituisce il film
         return $film; 
     }      
